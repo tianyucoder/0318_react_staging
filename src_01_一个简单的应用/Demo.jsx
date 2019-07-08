38c2e9a33073d4748cdb950d311db2102a04da6e
tianyu <tianyu@gmail.com>
2019-07-08 14:48:16 +0800

    init

---
+++ src_01_一个简单的应用/Demo.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import './Demo.css'
+
+export default class Demo extends Component{
+  render(){
+    return (
+      <h2 className="demo">下课了，我不困了</h2>
+    )
+  }
+}
