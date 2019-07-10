8507266135fd312c2d56d9388138c89e6afc1730
tianyu <tianyu@gmail.com>
2019-07-10 11:50:25 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React,{Component} from 'react'
+import {NavLink,Route,Redirect,Switch} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
 
 export default class App extends Component{
 
@@ -14,11 +17,13 @@ export default class App extends Component{
           <div className="col-xs-2 col-xs-offset-2">
             <div className="list-group">
 
-              {/*原生html中用a标签实现页面的切换，页面刷新，要准备多个页面*/}
+              {/*原生html中用a标签实现页面的切换，页面是刷新的，要准备多个页面*/}
               {/*<a className="list-group-item" href="./about.html">About</a>
               <a className="list-group-item active" href="./home.html">Home</a>*/}
 
-              {/*用react中的路由切换*/}
+              {/*用react中的路由切换------路由切换*/}
+              <NavLink className="list-group-item" activeClassName="demo" to='/about' >About</NavLink>
+              <NavLink className="list-group-item" activeClassName="demo" to='/home'>Home</NavLink>
 
 
             </div>
@@ -30,6 +35,14 @@ export default class App extends Component{
                 {/*即将展示的不同内容，对于原生html的写法，不同的内容就对应不同的页面*/}
                 {/*<h3>我是Home的内容</h3>
                 <h3>我是About的内容</h3>*/}
+
+                {/*定义（注册）路由与组件的对应关系*/}
+                <Switch>
+                  <Route path='/about' component={About}/>
+                  <Route path='/home' component={Home}/>
+                  <Redirect to='/about'/>
+                </Switch>
+
               </div>
             </div>
           </div>
